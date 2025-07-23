2e460b573523b8124a045ea30e371a6620e8eb1e
Nafis2003-ai <nafiskarim440080@gmail.com>
2025-07-23 09:05:45 +0600

    adding new files
    adding audio processing and editing, pitch changer,voice changer file in the project

--- codep/spd.py
+++ codep/spd.py
@@ -6,7 +6,7 @@ import gc
 import os
 
 
-HF_TOKEN = "hugging face token"  
+HF_TOKEN = "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"  
 path=input("Enter the path of the audio")
 # Initialize diarization pipeline
 pipeline = Pipeline.from_pretrained(
